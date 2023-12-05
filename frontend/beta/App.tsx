66f98a61e6523ffd389e90b289252a722797899d
snavemail <98936310+snavemail@users.noreply.github.com>
2023-12-05 03:12:19 -0500

    Change App.tsx to be SafeAreaView

--- frontend/beta/App.tsx
+++ frontend/beta/App.tsx
@@ -1,26 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import CameraComponent from './CameraComponent';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const Stack = createNativeStackNavigator();
-
-function DetailsScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-    </View>
-  );
-}
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-          <Stack.Screen name="Camera" component={CameraComponent} />
-        </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaView style={{ flex: 1 }}>
+      <CameraComponent/>
+    </SafeAreaView>
   );
 }
 
