2d5835fa7d8bbe300a7c048dc38f5d764b484a93
snavemail <98936310+snavemail@users.noreply.github.com>
2023-12-05 03:12:36 -0500

    UI to buttons and bottom bar on CameraComponent

--- frontend/beta/CameraComponent.tsx
+++ frontend/beta/CameraComponent.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { View, Text, Image, StyleSheet, Platform } from 'react-native';
-import { Button } from 'react-native-paper';
+import { View, Text, Image, StyleSheet, Platform, Button } from 'react-native';
+import { IconButton, MD3Colors } from 'react-native-paper';
 import { Camera } from 'expo-camera';
 import { TouchableOpacity } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
@@ -8,6 +8,8 @@ import * as ImagePicker from 'expo-image-picker';
 function CameraComponent() {
   const [hasPermission, setHasPermission] = useState<boolean | null>(null);
   const [image, setImage] = useState<string | null>(null);
+  const [cameraButtonDisabled, setCameraButtonDisabled] = useState<boolean>(false);
+  const [imageButtonDisabled, setImageButtonDisabled] = useState<boolean>(false);
   const cameraRef = useRef<Camera | null>(null);
   const address = '10.0.0.184'
 
@@ -37,13 +39,7 @@ function CameraComponent() {
     return <View><Text>No access to camera</Text></View>;
   }
 
-  const takePicture = async () => {
-    if (!cameraRef.current) {
-      console.error('Camera not available');
-      return;
-    }
-
-    const { uri } = await cameraRef.current.takePictureAsync();
+  const processImage = async (uri: any) => {
     const file = await fetch(uri);
     const blob = await file.blob();
     const reader = new FileReader();
@@ -63,60 +59,62 @@ function CameraComponent() {
         }
         const timestamp = Date.now();
         const url = `http://${address}:3000/get-image?timestamp=${timestamp}`
-        setImage(url)
+        
+        setImage(url);
+        setCameraButtonDisabled(false);
       })
       .catch(error => {
         console.error('Error sending image to server:', error);
         setImage(uri);
+        setCameraButtonDisabled(false);
       });
     };
     reader.readAsDataURL(blob);
+  }
+
+  /**
+   * Takes picture and then process it in the model via the server
+   * Disables the button
+   * @return nothing if the camera is not available
+   */
+  const takePicture = async () => {
+    if (!cameraRef.current) {
+      console.error('Camera not available');
+      return;
+    }
+    setCameraButtonDisabled(true);
+    const { uri } = await cameraRef.current.takePictureAsync();
+    processImage(uri);
   };
 
+  /**
+   * Set the image to be null
+   */
   const retakePicture = () => {
     setImage(null);
   };
   
+  /**
+   * Go through image picker to select an image (video doesn't work, although we allow it)
+   * Will remove in future possibly
+   * Disables camera button as well
+   */
   const pickImage = async () => {
+    setCameraButtonDisabled(true);
+    setImageButtonDisabled(true);
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
       allowsEditing: true,
-      aspect: [16, 9],
-      quality: 1,
+      aspect: [4, 3],
+      quality: .5,
     });
-
-    console.log(result);
 
     if (!result.canceled) {
       const uri = (result.assets[0].uri)
-      const file = await fetch(uri);
-      const blob = await file.blob();
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        const base64 = reader.result;
-        const formData = new FormData();
-        formData.append('image', base64 as string);
-    
-        fetch(`http://${address}:3000/predict`, {
-          method: 'POST',
-          body: formData,
-          headers: { 'Content-Type': "image/jpeg", },
-        })
-        .then(response => {
-          if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-          }
-          const timestamp = Date.now();
-          const url = `http://${address}:3000/get-image?timestamp=${timestamp}`
-          setImage(url)
-        })
-        .catch(error => {
-          console.error('Error sending image to server:', error);
-          setImage(uri);
-        });
-      };
-      reader.readAsDataURL(blob);
+      await processImage(uri);
     }
+    setImageButtonDisabled(false);
+    setCameraButtonDisabled(false);
   };
 
 
@@ -130,16 +128,12 @@ function CameraComponent() {
           </TouchableOpacity>
         </View>
       ) : (
-        // Display the camera for taking a new picture
-        <Camera ref={(ref) => (cameraRef.current = ref)} style={{ flex: 1 }}>
-          <View style={{ flex: 1, backgroundColor: 'transparent', flexDirection: 'row' }}>
+        <Camera ref={cameraRef} style={{ flex: 1 }}>
+          <View style={{ flex: 1, backgroundColor: 'transparent', flexDirection: 'row' }} />
+          <View style={styles.bottomBar}>
+            <IconButton icon={"image"} onPress={pickImage} iconColor={MD3Colors.neutral70} mode={"contained-tonal"} disabled={imageButtonDisabled}/>
+            <IconButton icon='camera' onPress={takePicture} iconColor={MD3Colors.neutral70} mode={"contained-tonal"} disabled={cameraButtonDisabled}/>
           </View>
-          <Button icon={'camera'} onPress={takePicture} style={{ marginBottom: 40 }}>
-            Pic
-          </Button>
-          <Button icon={'camera'} onPress={pickImage} style={{ marginBottom: 40 }}>
-            Roll
-          </Button>
         </Camera>
       )}
     </View>
@@ -153,6 +147,12 @@ const styles = StyleSheet.create({
     backgroundColor: 'blue',
     padding: 10,
     borderRadius: 10,
+  },
+  bottomBar: {
+    backgroundColor: 'black',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 20,
   },
 });
 
