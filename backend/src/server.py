f98086426b9eb561c6a3bf90d7e1d7c372232f47
femailman <93400575+femailman@users.noreply.github.com>
2023-12-17 00:20:03 -0500

    Add 2 API calls for getting route of a color

--- backend/src/server.py
+++ backend/src/server.py
@@ -1,18 +1,13 @@
-import platform
 from flask import Flask, request, jsonify, send_file
+import numpy as np
 from ultralytics import YOLO
-import base64
-import os
-import utils as Util
-import pathlib
-
-plt = platform.system()
-if plt != "Windows":
-    pathlib.WindowsPath = pathlib.PosixPath
+import base64, os, utils as Util, pathlib, platform, json
+from classes.ImageAttribute import ImageAttribute
+from classes.Hold import Hold
 
 app = Flask(__name__)
 
-difficulty_classifier_model = YOLO("backend/models/classify/classifyv2.pt")
+difficulty_classifier_model = YOLO("backend/models/classify/classifyv3.pt")
 hold_detection_model = YOLO("backend/models/detect/detectv1.pt")
 
 
@@ -61,19 +56,61 @@ def predict_difficulty():
 
         img = Util.get_image(os.path.join("backend/react_images/classify", "image.jpg"))
         results = difficulty_classifier_model.predict(img)
-        Util.disp_results(results, "classify")
-
-        print(Util.get_difficulty(results))
+        Util.disp_results(results=results, folder="classify")
         return jsonify({"difficulty": Util.get_difficulty(results)})
 
     except Exception as e:
-        print("Exception")
+        return jsonify({"status": "error", "message": str(e)})
+
+
+@app.route("/get-color-holds", methods=["POST"])
+def get_color_holds():
+    """
+    Takes in an image and returns all of the holds from the image of a certain color
+    """
+    try:
+        if "image" not in request.form:
+            raise ValueError("Missing 'image' in form data", request.form)
+        image_data = request.form.get("image")
+        image_data = image_data.split(",")[1]
+        image_data = base64.b64decode(image_data)
+        hsvr_json = request.form.get("hsvr")
+        hsvr_list = json.loads(hsvr_json)
+
+        with open(
+            os.path.join("backend/react_images/route-finder", "image.jpg"), "wb"
+        ) as f:
+            f.write(image_data)
+
+        img_path = os.path.join("backend/react_images/route-finder", "image.jpg")
+        print("working")
+        holds = Util.get_holds_array(
+            img_path, [hsvr_list[0], hsvr_list[1], hsvr_list[2]], hsvr_list[3]
+        )
+
+        # image_attribute = ImageAttribute(path=img_path)
+        print("good")
+        return jsonify({"status": "success"})
+    except Exception as e:
         return jsonify({"status": "error", "message": str(e)})
 
 
 @app.route("/get-image", methods=["GET"])
 def get_image():
-    processed_image_path = os.path.join("react_images/classify", "result.jpg")
+    base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+    processed_image_path = os.path.join(
+        base_dir, "react_images", "classify", "result.jpg"
+    )
+    return send_file(processed_image_path, mimetype="image/jpeg")
+
+
+@app.route("/get-color-image", methods=["GET"])
+def get_color_image():
+    base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+    processed_image_path = os.path.join(
+        base_dir, "react_images", "route-finder", "result.jpg"
+    )
+    print("sending file")
     return send_file(processed_image_path, mimetype="image/jpeg")
 
 
